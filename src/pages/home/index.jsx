68d3f96455f491ad32a2132d3d4a3154bda3b6ec
jm.yeye <yeye.jean_marc@live.fr>
2023-01-21 12:23:08 +0100

    convert year+date

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,97 @@
 import React, { useState } from 'react';
 
+let romanNumeral = '';
+const romanNumeralMap = {
+  1000: 'M',
+  900: 'CM',
+  800: 'DCCC',
+  700: 'DCC',
+  600: 'DC',
+  500: 'D',
+  400: 'CD',
+  100: 'C',
+  90: 'XC',
+  80: 'LXXX',
+  70: 'LXX',
+  60: 'LX',
+  50: 'L',
+  40: 'XL',
+  10: 'X',
+  9: 'IX',
+  8: 'VIII',
+  7: 'VII',
+  6: 'VI',
+  5: 'V',
+  4: 'IV',
+  3: 'III',
+  2: 'II',
+  1: 'I',
+};
+
+function splitDate(inputValue) {
+  const dateArray = inputValue.split('/');
+  if (dateArray.length === 3) {
+    return dateArray;
+  }
+
+  return inputValue;
+}
+
+function convertNumber(numberToConvert) {
+  if (numberToConvert < 100) {
+    return convertTens(numberToConvert);
+  } else if (numberToConvert < 1000) {
+    return convertHundreds(numberToConvert);
+  }
+
+  return convertThousands(numberToConvert);
+}
+
+
+function convertTens(numberToConvert) {
+  const units = numberToConvert % 10;
+  const tens  = (numberToConvert - units) / 10;
+  const convertedNumber = tens * 10;
+  if (romanNumeralMap[convertedNumber] !== undefined) {
+    romanNumeral+=romanNumeralMap[convertedNumber]
+  } else {
+    for (let i = 0; i < tens; i++) {
+      romanNumeral+=romanNumeralMap[10]
+    }
+  }
+
+  romanNumeral+=romanNumeralMap[units];
+}
+
+function convertHundreds(numberToConvert) {
+  const tens = numberToConvert % 100;
+  const hund  = (numberToConvert - tens) / 100;
+  const convertedNumber = hund * 100;
+  if (romanNumeralMap[convertedNumber] !== undefined) {
+    romanNumeral+=romanNumeralMap[convertedNumber]
+  } else {
+    for (let i = 0; i < hund; i++) {
+      romanNumeral+=romanNumeralMap[100]
+    }
+  }
+  convertTens(tens);
+}
+
+function convertThousands(numberToConvert) {
+  const hund = numberToConvert % 1000;
+  const thous  = (numberToConvert - hund) / 1000;
+  const convertedNumber = thous * 1000;
+  if (romanNumeralMap[convertedNumber] !== undefined) {
+    romanNumeral+=romanNumeralMap[convertedNumber]
+  } else {
+    for (let i = 0; i < thous; i++) {
+      romanNumeral+=romanNumeralMap[1000]
+    }
+  }
+
+  convertHundreds(hund);
+}
+
 const RomanNumeralConverter = () => {
   const [inputValue, setInputValue] = useState('');
   const [outputValue, setOutputValue] = useState('');
@@ -9,76 +101,18 @@ const RomanNumeralConverter = () => {
   };
 
   const handleConvertClick = () => {
-    const romanNumeralMap = {
-      1000: 'M',
-      900: 'CM',
-      800: 'DCCC',
-      700: 'DCC',
-      600: 'DC',
-      500: 'D',
-      400: 'CD',
-      100: 'C',
-      90: 'XC',
-      80: 'LXXX',
-      70: 'LXX',
-      60: 'LX',
-      50: 'L',
-      40: 'XL',
-      10: 'X',
-      9: 'IX',
-      8: 'VIII',
-      7: 'VII',
-      6: 'VI',
-      5: 'V',
-      4: 'IV',
-      3: 'III',
-      2: 'II',
-      1: 'I',
-    };
 
-    let number = parseInt(inputValue);
-    let romanNumeral = '';
-    let diz = 0;
-    let unit = 0;
-    let cent = number % 1000;
-    const thous  = (number - cent) / 1000;
-    for (let i = 0; i < thous; i++) {
-      romanNumeral+=romanNumeralMap[1000]
+
+    let number = splitDate(inputValue);
+    if (Array.isArray(number)) {
+      number.map((element) => {
+        return convertNumber(parseInt(element));
+      })
+    } else {
+      convertNumber(parseInt(number));
     }
-
-    if (cent > 0) {
-      diz = cent % 100;
-      cent = (cent - diz) / 100;
-      const convertToCent = cent * 100;
-      if (romanNumeralMap[convertToCent] !== undefined) {
-        romanNumeral+=romanNumeralMap[convertToCent]
-      } else {
-        for (let i = 0; i < cent; i++) {
-          romanNumeral += romanNumeralMap[100]
-        }
-      }
-    }
-
-    if (diz > 0) {
-      unit = diz % 10;
-      diz = (diz - unit) / 10;
-      const convertToDiz = diz * 10;
-      if (romanNumeralMap[convertToDiz] !== undefined) {
-        romanNumeral+=romanNumeralMap[convertToDiz]
-      } else {
-        for (let i = 0; i < cent; i++) {
-          romanNumeral += romanNumeralMap[10]
-        }
-      }
-    }
-
-    if (unit > 0) {
-      if (romanNumeralMap[unit] !== undefined) {
-        romanNumeral+=romanNumeralMap[unit]
-      }
-    }
-
-    setOutputValue(romanNumeral);
+console.log(romanNumeral);
+    setOutputValue('');
   };
 
   return (
