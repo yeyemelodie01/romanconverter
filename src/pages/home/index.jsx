b53ae0f733a3a8cd59c71a8dc795f7c7c75473cf
melodieyeye <yeyemelodie@outlook.fr>
2023-02-22 11:41:51 +0100

    add modifications

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,25 +6,46 @@ function Home() {
   return (
       <div className="parentGrid">
         <div className="firstChildGrid">
-          <h1 className="titleHome">Convertisseur de Chiffre Romain</h1>
-          <div className="divInputNumber">
-            <h2 className="subTitleHome">Convertisser votre nombre puis enregistrer-le en PDF</h2>
-            <div>
-              <label form='number'/>
-              <input id="number" className="inputStyle" placeholder="Entrez votre nombre" type="text"/>
-              <button className="btnInput">Convertir</button>
+          <div>
+            <h1 className="titleHome">Romanator</h1>
+            <div className="divInputNumber">
+              <h2 className="subTitleHome">Convertisser votre nombre</h2>
+              <div>
+                <label form='number'/>
+                <input id="number" className="inputStyle" placeholder="Entrez votre nombre" type="text"/>
+                <button className="btnInput">Convertir</button>
+              </div>
             </div>
           </div>
         </div>
         <div className="secondChildGrid">
-          <div className="contentFlex">
-            <div className="normalNumber">1091993</div>
-            <div>
-              <div className="border widthYear"/>
-              <div className="romanNumber">IIXMCMXCIII</div>
+          <div className="divSecondGrid">
+            <div className="divFlexSecond">
+              <div className="contentFlex">
+                <div className="normalNumber">1</div>
+                <div>
+                  <div className="border"/>
+                  <div className="romanNumber">I</div>
+                </div>
+              </div>
+              <div className="contentFlex">
+                <div className="normalNumber">09</div>
+                <div>
+                  <div className="border"/>
+                  <div className="romanNumber">IX</div>
+                </div>
+              </div>
+              <div className="contentFlex">
+                <div className="normalNumber">1993</div>
+                <div>
+                  <div className="border widthYear"/>
+                  <div className="romanNumber">MCMXCIII</div>
+                </div>
+              </div>
             </div>
             <button className="btnRegister">Enregistrer en .PDF</button>
           </div>
+
         </div>
       </div>
   );
