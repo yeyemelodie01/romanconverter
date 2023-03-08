1ee1401ef86f6f7afc1c6ab5311d4b60373fcc1c
melodieyeye <yeyemelodie@outlook.fr>
2023-03-08 08:55:10 +0100

    add modifications

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,29 +1,27 @@
 import { useState } from 'react'
 import '../../utils/styles/header.css'
 import '../../utils/styles/bouton.css'
-import France from '../../assets/french-guiana.svg'
-import USA from '../../assets/united-states.svg'
-import arrowDown from '../../assets/angle-arrow-down_icon-icons.com_73683.svg'
+//import France from '../../assets/french-guiana.svg'
+//import USA from '../../assets/united-states.svg'
+//import arrowDown from '../../assets/angle-arrow-down_icon-icons.com_73683.svg'
 
 function Header(){
   const [ showBurger, setShowBurger ] = useState(false);
 
-  function BtnChangeLangue() {
-    let list = document.getElementById('listScrolling');
-    list.style.display = { 'block': 'none' };
-  }
+  // function BtnChangeLangue() {
+  //   let list = document.getElementById('listScrolling');
+  //   list.style.display = { 'block': 'none' };
+  // }
 
   return(
       <header>
         <nav>
-          <div className="divImg">
-            <h1>XXIX</h1>
+          <h1 className="logo">XXIX</h1>
+          <div className="divRoute">
             <ul className="listFlex">
-              <div className="divFlexLi">
-                <li className="liSize">ACCUEIL</li>
-                <li><span>|</span></li>
-                <li className="liSize">À PROPOS</li>
-              </div>
+              <li className="liSize">ACCUEIL</li>
+              <li><span className="trait">|</span></li>
+              <li className="liSize">À PROPOS</li>
             </ul>
           </div>
           <div>
